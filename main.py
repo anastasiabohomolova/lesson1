cda9321bd4fa0b31180c29bb431782989b160cde
Anastasia <136612325+anastasiabohomolova@users.noreply.github.com>
2023-06-15 09:40:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#a = input('')
+#b = int(a)
+#print(type(b))
+#c = id(b)
+#print(c)
+
+#d = '3', '5', '6'
+#g = int(d)
+#print(g)
+
+l = input('')
+o = input('')
+i = input('')
+a = l + o + i
+b = int(a)
+print(type(b))
+c = id(b)
+print(c)
